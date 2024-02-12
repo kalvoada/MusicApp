c8dfcfcf82b2b32f738145f143df6ec49b5076b1
kalvoada <kalvoada@fit.cvut.cz>
2024-02-12 21:35:20 +0100

    add template

--- culture/culture/settings.py
+++ culture/culture/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_bootstrap4',
 
 
     "django.contrib.admin",
@@ -76,7 +77,7 @@ ROOT_URLCONF = "culture.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -138,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
